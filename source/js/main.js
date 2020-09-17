99e2bd30b545acba84d6d1608895691d7856a824
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2020-09-17 13:43:03 +0300

    Закончил разметку главной страницы

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,27 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper_1 = new Swiper('.swiper-container-1', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-1',
     clickable: true,
   },
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+});
+
+var swiper_2 = new Swiper('.swiper-container-2', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination-2',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
 });
