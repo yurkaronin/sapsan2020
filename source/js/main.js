75faed89f8db0d67980191186a61d396cb9b5e25
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2020-10-05 05:17:58 +0300

    Сверстал страничку карточка каталога

--- source/js/main.js
+++ source/js/main.js
@@ -26,6 +26,43 @@ var swiper_2 = new Swiper('.swiper-container-2', {
   }, */
 });
 
+var swiper_3 = new Swiper('.swiper-container-3', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  /* autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }, */
+});
+
+/* Слайдер в карточке товара  */
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  loop: true,
+  freeMode: true,
+  loopedSlides: 5, //looped slides should be the same
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  loop: true,
+  loopedSlides: 5, //looped slides should be the same
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
+
 /* Бургер меню  */
 $(document).ready(function() {
 	$('#nav-icon').click(function() {
